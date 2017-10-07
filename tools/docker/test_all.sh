fc77078721e27e098166ddae1fe788c11c89b0f3
hblok <gitmail@hblok.net>
2017-10-07 09:28:31 +0200

    Try Travis again

---
+++ tools/docker/test_all.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+TEST_OPTIONS="--force_python py3 --python_path /usr/bin/python3 --test_output=errors --verbose_failures --genrule_strategy=standalone --spawn_strategy=standalone"
+bazel --batch test ${TEST_OPTIONS} ...
--- tools/docker/trusty/bazel_examples_test.sh
+++ tools/docker/trusty/bazel_examples_test.sh
@@ -7,6 +7,4 @@ export PATH=/usr/local/node_modules/mocha/bin:$PATH
 git clone https://github.com/hblok/bazel_examples.git
 cd bazel_examples
 
-TEST_OPTIONS="--force_python py3 --python_path /usr/bin/python3 --test_output=errors --verbose_failures --genrule_strategy=standalone --spawn_strategy=standalone"
-bazel --batch test ${TEST_OPTIONS} ...
-
+tools/docker/test_all.sh
