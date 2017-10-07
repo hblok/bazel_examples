a2b1ab39925b170638225b5ab33d025d1a2f21bc
hblok <gitmail@hblok.net>
2017-10-07 10:02:09 +0200

    set PATH in right script

--- tools/docker/test_all.sh
+++ tools/docker/test_all.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+export PATH=/usr/local/node_modules/mocha/bin:$PATH
+
 TEST_OPTIONS="--force_python py3 --python_path /usr/bin/python3 --test_output=errors --verbose_failures --genrule_strategy=standalone --spawn_strategy=standalone"
 bazel --batch test ${TEST_OPTIONS} ...
--- tools/docker/trusty/bazel_examples_test.sh
+++ tools/docker/trusty/bazel_examples_test.sh
@@ -2,10 +2,6 @@
 
 cd `dirname $0`
 
-FOUND_MOCHA=$(dirname `find / -path "*/mocha/bin/mocha"`)
-echo "Found Mocha at $FOUND_MOCHA"
-export PATH=$FOUND_MOCHA:/usr/local/node_modules/mocha/bin:$PATH
-
 git clone https://github.com/hblok/bazel_examples.git
 cd bazel_examples
 
